59aad7cd34f50f721f386ef169892552100d7746
Bhargav <bhargavkr2000@gmail.com>
2021-06-21 12:03:37 +0530

    Threads

---
+++ src/com/java/multithreading/Mytask.java
@@ -0,0 +1,29 @@
+package com.java.multithreading;
+
+class MultiThreading {
+    public void execute(){
+        System.out.println("Executing the method...");
+    }
+}
+
+class Mytask2 extends MultiThreading implements Runnable{
+    @Override
+    public void run() {
+        super.execute();
+        for(int i = 0;i < 10 ;i++){
+            System.out.println("^^ Printing document "+i+" from printer 3 ");
+        }
+    }
+}
+
+public class Mytask extends Thread {
+
+    @Override
+    public void run() {
+        for(int i = 0;i < 10 ;i++){
+            System.out.println("@@ Printing document "+i+" from printer 1 ");
+        }
+    }
+}
+
+
